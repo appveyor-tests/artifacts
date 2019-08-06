a98a419b8cce6708a2d09d51ad0b3c122a22c026
Alena Hrynkevich <alena@appveyor.com>
2019-08-06 11:31:06 -0700

    Calling the mklink by cmd

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
-echo appveyor version
+echo "appveyor version"
 $ErrorActionPreference = "Stop"
 (New-Object Net.WebClient).DownloadString('https://www.appveyor.com/tools/my-ip.aspx')
 
@@ -8,7 +8,7 @@ $ErrorActionPreference = "Stop"
 #- appveyor DownloadFile http://nsis.sourceforge.net/mediawiki/images/2/28/Nsisos.zip -FileName Nsisos.zip
 
 #- dir c:\go16\doc
-echo Pushing artifacts
+echo "Pushing artifacts"
 Push-AppveyorArtifact C:\go16\doc\go1.html -FileName 'go - file - with spaces, v. 1.html'
 Push-AppveyorArtifact C:\go16\doc\go1.1.html
 Push-AppveyorArtifact C:\go16\doc\go1.2.html
@@ -18,6 +18,7 @@ Push-AppveyorArtifact C:\go16\doc\go1.5.html
 Push-AppveyorArtifact C:\go16\doc\go1.6.html
 
 # download bunch of files in a row using PS
+echo "Download bunch of files in a row using PS"
 Start-FileDownload 'https://dl.dropboxusercontent.com/s/c1by0uulee6g8th/wxWidgets.7z?dl=0' -FileName wxWidgets.7z
 Start-FileDownload 'https://drive.google.com/uc?export=download&id=0B-98fOyaZKJ5YWVnb29JZXFQWkU' -FileName llvmlibs.7z
 Start-FileDownload 'https://drive.google.com/uc?export=download&id=0B8A6NaxhQAGRa21fbDQteTN1dGs' -FileName vulkan.7z
@@ -26,6 +27,7 @@ Start-FileDownload https://www.appveyor.com/downloads/Appveyor.NUnitLogger.zip
 Start-FileDownload https://www.appveyor.com/downloads/Appveyor.NUnit3Logger.zip
 
 # download bunch of files in a row using appveyor.exe
+echo "Download bunch of files in a row using appveyor.exe"
 appveyor DownloadFile "https://dl.dropboxusercontent.com/s/c1by0uulee6g8th/wxWidgets.7z?dl=0" -FileName wxWidgets.7z
 appveyor DownloadFile "https://drive.google.com/uc?export=download&id=0B-98fOyaZKJ5YWVnb29JZXFQWkU" -FileName llvmlibs.7z
 appveyor DownloadFile "https://drive.google.com/uc?export=download&id=0B8A6NaxhQAGRa21fbDQteTN1dGs" -FileName vulkan.7z
@@ -46,9 +48,9 @@ copy go17.zip go17-2.zip
 Push-AppveyorArtifact go17-1.zip
 Start-FileDownload https://nodejs.org/dist/v6.5.0/node-v6.5.0-darwin-x64.tar.gz -FileName "$env:appveyor_build_folder\node-v6.5.0.zip" -Timeout 1000
 appveyor DownloadFile https://nodejs.org/dist/v6.6.0/node-v6.6.0-darwin-x64.tar.gz -Timeout 1000
-mklink go17-1-link.zip go17-1.zip
+cmd /c mklink go17-1-link.zip go17-1.zip
 Push-AppveyorArtifact go17-1-link.zip
-mklink go17-2-link.zip go17-1.zip
+cmd /c mklink go17-2-link.zip go17-1.zip
 move C:\go16 %appveyor_build_folder%\go16
 appveyor PushArtifact README.md -FileName README-2.md -Verbosity Normal
 
